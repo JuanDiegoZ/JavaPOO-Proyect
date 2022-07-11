eb5bbdfb4ada8d97c2642897742afd43cb40c922
juanZipaquira <juandi0055@hotmail.com>
2022-07-11 16:00:49 -0500

    Creando primera clase(POO) en Java

---
+++ JavaClass/src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+
+}
+// declarar un objeto:        Doctor  myDoctor;
+// Instanciando el objeto:    myDoctor = new Doctor;
